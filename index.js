e7c08699ba0e98534478f6ebe3a3649b92230acf
Bob <bkane90@gmail.com>
2020-11-09 12:06:34 -0500

    added-road

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
 const worldImg = new Image()
-worldImg.src = `./worldimage.png`
+worldImg.src = `./images/bezos-background.jpg`
 
 let world = { 
     x: 0,
     y: 0, 
     w: canvas.width , 
-    h: canvas.height *.5 }
+    h: canvas.height * 0.65}
 
 worldImg.onload = function () {
     ctx.drawImage(worldImg, world.x, world.y, world.w, world.h)

e29acc06f531f81824b67c4d417bbae0355836cc
aparr075 <aparr075@fiu.edu>
2020-11-09 17:15:32 -0500

    obstacles

--- index.js
+++ index.js
@@ -36,7 +36,55 @@ class bot {
   }
 }
 
-  let Bezos = new bot(0, 340, 250, 175)
+let Bezos = new bot(0, 340, 250, 175)
+
+
+let muskImg = new Image()
+muskImg.src ="./images/musk.png"
+
+let musk = []
+
+let zuckImg = new Image()
+zuckImg.src = "./images/zuck.png"
+
+
+class Obstacles {
+  constructor(x,y,w,h){
+      this.x = x;
+      this.y = y;
+      this.w = w;
+      this.h = h;
+  }
+  drawObstacles() {
+      this.x--
+      ctx.drawImage(muskImg, this.x, this.y, this.w, this.h)
+      ctx.drawImage(zuckImg, this.x-100, this.y+75, this.w, this.h)
+  }
+
+}
+
+setInterval(function () {
+  let newObs = new Obstacles(1000,300,muskImg.width * .45,muskImg.height *.45)
+  musk.push(newObs)
+}, 10000)
+
+function drawMusk(){
+   for(let elon of musk){
+       elon.drawObstacles()
+   }
+  }
+
+function drawZuck(){
+  for(let mark of zuck){
+    mark.drawObstacles()
+  }
+}
+
+function drawCook(){
+  for(let tim of cook){
+    tim.drawObstacles()
+  }
+}
 
 animationId = null;
 
@@ -45,5 +93,6 @@ function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   ctx.drawImage(worldImg, world.x, world.y, world.w, world.h)
   ctx.drawImage(BezosImg, Bezos.x, Bezos.y, Bezos.w, Bezos.h)
+  drawMusk()
 }
 animate()

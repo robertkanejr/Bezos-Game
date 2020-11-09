95109cfbcdcb8e59d2f63c6deb34c10386e5510d
Cameron Campbell <cameroncampbell@camerons-air.attlocal.net>
2020-11-09 13:08:49 -0500

    fixed the instructions

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
-
 const worldImg = new Image()
 worldImg.src = `./images/bezos-background.jpg`
 
-let world = { 
-    x: 0,
-    y: 0, 
-    w: canvas.width , 
-    h: canvas.height * 0.65}
+canvas.width = window.innerWidth;
+canvas.height = canvas.width * worldImg.height / worldImg.width
+console.log(canvas.height)
+const ctx = canvas.getContext('2d')
+
+
+
+
+let world = {
+  x: 0,
+  y: 0,
+  w: canvas.width,
+  h: canvas.height
+}
 
 worldImg.onload = function () {
-    ctx.drawImage(worldImg, world.x, world.y, world.w, world.h)
-  }
+  ctx.drawImage(worldImg, world.x, world.y, world.w, world.h)
+}
 
 
-  
+
 animationId = null;
 
 function animate() {
-    animationId = requestAnimationFrame(animate)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.drawImage(worldImg, world.x, world.y, world.w, world.h)
-  
-  
-  }
-  animate()
+  animationId = requestAnimationFrame(animate)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.drawImage(worldImg, world.x, world.y, world.w, world.h)
+
+
+}
+animate()

890fb6fde7415f866bf2cdce2aaeb1ab80ab9518
Ana Parra <aparr075@fiu.edu>
2020-11-09 10:05:59 -0500

    background

--- index.js
+++ index.js
@@ -2,4 +2,27 @@ const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
 const worldImg = new Image()
-worldImg.src = ``
+worldImg.src = `./worldimage.png`
+
+let world = { 
+    x: 0,
+    y: 0, 
+    w: canvas.width , 
+    h: canvas.height *.5 }
+
+worldImg.onload = function () {
+    ctx.drawImage(worldImg, world.x, world.y, world.w, world.h)
+  }
+
+
+  
+animationId = null;
+
+function animate() {
+    animationId = requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.drawImage(worldImg, world.x, world.y, world.w, world.h)
+  
+  
+  }
+  animate()
